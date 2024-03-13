143d2b16974032a6f89d2b9e7fa23b5068a6b0dc
sai <dodda.saiteja123@gmail.com>
2024-03-13 09:50:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "@mui/material";
+import { darkTheme } from "./theme/darktheme";
+import Navbar from "./Page/Navbar/Navbar";
+import Home from "./Page/Home/Home";
+import Auth from "./Page/Auth/Auth";
+import { useDispatch, useSelector } from "react-redux";
+import store from "./ReduxToolKit/Store";
+import { fetchTasks } from "./ReduxToolKit/TaskSlice";
+import { getUserProfile } from "./ReduxToolKit/AuthSlice";
 
 function App() {
+  const user = true;
+  const dispatch=useDispatch();
+  const {task,auth}=useSelector(store=>store);
+
+  useEffect(()=>{
+    dispatch(fetchTasks({}));
+    dispatch(getUserProfile(auth.jwt || localStorage.getItem("jwt")));
+  },[auth.jwt])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      {auth.user ? 
+        <div>
+          <Navbar />
+          <Home />
+        </div>
+            : 
+        <Auth />
+      }
+    </ThemeProvider>
   );
 }
 
